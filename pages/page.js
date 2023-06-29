a7e08739f5e09be36f86569c57ee6a3956538fb1
Banitongwa David <davidbanitongwa@gmail.com>
2023-06-29 23:25:09 +0100

    coming soon

---
+++ pages/page.js
@@ -0,0 +1,10 @@
+import styles from './page.module.css'
+import ComingSoon from '@/components/ComingSoon'
+
+export default function Home() {
+  return (
+    <>
+    <ComingSoon />
+    </>
+  )
+}
