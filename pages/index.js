e26070de62ca98d68ad3d2ac3b3e24575282b473
Banitongwa David <davidbanitongwa@gmail.com>
2023-06-30 11:14:00 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,6 @@ import ComingSoon from "@/components/ComingSoon";
 import RootLayout from "./layout";
 
 export default function Home() {
-  /*
- <Head>
-      <title>Portfolio David</title>
-      <meta name='description' content='Personal Portfolio create with Next JS' />
-      <link rel='icon' href='favicon.ico' />
-    </Head>
-  */
   return (
     <RootLayout>
       <ComingSoon />
