e8b0db3c6ba409f5830a2ebe2d54d7897adc630b
Banitongwa David <davidbanitongwa@gmail.com>
2023-06-30 22:32:58 +0100

    update head

--- pages/layout.js
+++ pages/layout.js
@@ -5,16 +5,16 @@ const inter = Inter({ subsets: ["latin"] });
 export default function RootLayout({ children }) {
   const metadata = {
     title: "Portfolio David",
-    description: "Personal Portfolio create with Next JS",
+    description: "Personal Portfolio created by David Banitongwa",
   };
 
   return (
     <>
       <Head>
-        <title>Portfolio David</title>
+        <title>Portfolio | David</title>
         <meta name="title" content={metadata.title} />
         <meta name="description" content={metadata.description} />
-        <link rel="icon" href="favicon.ico" />
+        <link rel="icon" href="profile.png" />
       </Head>
       <div className={inter.className}>{children}</div>
     </>
