6bcf4d8ddfeac100964cfa12e59231177d0a0a97
Banitongwa David <davidbanitongwa@gmail.com>
2023-07-15 22:03:40 +0100

    footer update

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -10,31 +10,31 @@ export default function Footer() {
                 href="#"
                 className="text-base leading-6 text-gray-500 hover:text-white"
               >
+                Home
+              </a>
+            </div>
+            <div className="px-5 py-2">
+              <a
+                href="#"
+                className="text-base leading-6 text-gray-500 hover:text-white"
+              >
+                Project
+              </a>
+            </div>
+            <div className="px-5 py-2">
+              <a
+                href="#"
+                className="text-base leading-6 text-gray-500 hover:text-white"
+              >
+                Skills
+              </a>
+            </div>
+            <div className="px-5 py-2">
+              <a
+                href="#"
+                className="text-base leading-6 text-gray-500 hover:text-white"
+              >
                 About
-              </a>
-            </div>
-            <div className="px-5 py-2">
-              <a
-                href="#"
-                className="text-base leading-6 text-gray-500 hover:text-white"
-              >
-                Blog
-              </a>
-            </div>
-            <div className="px-5 py-2">
-              <a
-                href="#"
-                className="text-base leading-6 text-gray-500 hover:text-white"
-              >
-                Team
-              </a>
-            </div>
-            <div className="px-5 py-2">
-              <a
-                href="#"
-                className="text-base leading-6 text-gray-500 hover:text-white"
-              >
-                Pricing
               </a>
             </div>
             <div className="px-5 py-2">
@@ -45,17 +45,9 @@ export default function Footer() {
                 Contact
               </a>
             </div>
-            <div className="px-5 py-2">
-              <a
-                href="#"
-                className="text-base leading-6 text-gray-500 hover:text-white"
-              >
-                Terms
-              </a>
-            </div>
           </nav>
           <div className="flex justify-center mt-8 space-x-6">
-            <a href="#" className="text-gray-400 hover:text-gray-500">
+            <a href="#" className="text-gray-400 hover:text-[#43d3e9]">
               <span className="sr-only">Facebook</span>
               <svg
                 className="w-6 h-6"
@@ -70,7 +62,7 @@ export default function Footer() {
                 ></path>
               </svg>
             </a>
-            <a href="#" className="text-gray-400 hover:text-gray-500">
+            <a href="#" className="text-gray-400 hover:text-[#43d3e9]">
               <span className="sr-only">Twitter</span>
               <svg
                 className="w-6 h-6"
@@ -81,7 +73,7 @@ export default function Footer() {
                 <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"></path>
               </svg>
             </a>
-            <a href="#" className="text-gray-400 hover:text-gray-500">
+            <a href="#" className="text-gray-400 hover:text-[#43d3e9]">
               <span className="sr-only">GitHub</span>
               <svg
                 className="w-6 h-6"
@@ -96,7 +88,7 @@ export default function Footer() {
                 ></path>
               </svg>
             </a>
-            <a href="#" className="text-gray-400 hover:text-gray-500">
+            <a href="#" className="text-gray-400 hover:text-[#43d3e9]">
               <span className="sr-only">Dribbble</span>
               <svg
                 className="w-6 h-6"
