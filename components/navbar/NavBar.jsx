88409e90f3eb132331dbbcc6cf75b60fc99d6c03
David BANITONGWA <davidbanitongwa@gmail.com>
2023-12-19 15:43:53 +0100

    update NavBar.jsx and globals.css

--- components/navbar/NavBar.jsx
+++ components/navbar/NavBar.jsx
@@ -1,11 +1,45 @@
 import Link from "next/link";
-import React, { useRef } from "react";
+import React, { useRef, useEffect } from "react";
 import { FaLinkedin, FaGithub, FaSquareXTwitter } from "react-icons/fa6";
 import SocialLinks from "@/data/SocialLinks";
 
 
 export default function NavBar() {
     const navbar = useRef(null);
+
+    useEffect(() => {
+        const pointer = document.createElement("DIV");
+        pointer.id = "mouse-pointer";
+        const centerPointer = document.createElement("DIV");
+        centerPointer.id = "center-point";
+        document.body.appendChild(pointer);
+        document.body.appendChild(centerPointer);
+    
+        document.onmousemove = function (event) {
+          requestAnimationFrame(() => {
+            centerPointer.style.left = event.clientX - 2 + "px";
+            centerPointer.style.top = event.clientY - 2 + "px";
+          });
+          setTimeout(() => {
+            requestAnimationFrame(() => {
+              pointer.style.left = event.clientX - 25 + "px";
+              pointer.style.top = event.clientY - 25 + "px";
+            });
+          }, 100);
+        };
+    
+        document.onmousedown = function (event) {
+          requestAnimationFrame(() => {
+            centerPointer.style.transform = "scale(1.3)";
+          });
+        };
+    
+        document.onmouseup = function (event) {
+          requestAnimationFrame(() => {
+            centerPointer.style.transform = "scale(1)";
+          });
+        };
+      }, []);
 
   const handleChangeMenu = (index) => {
       let liTags = navbar.current.children
