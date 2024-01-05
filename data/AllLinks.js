74035c7e5a6584855726ea10d612cbf89147a3f2
David BANITONGWA <davidbanitongwa@gmail.com>
2024-01-05 16:14:25 +0100

    update alllinks

--- data/AllLinks.js
+++ data/AllLinks.js
@@ -1,7 +1,7 @@
 const AllLinks = {
-    linkedin: "https://www.linkedin.com/",
-    github: "https://github.com/",
-    twitter: "https://twitter.com/",
+    linkedin: "https://www.linkedin.com/in/david-banitongwa/",
+    github: "https://github.com/Banitongwa7",
+    twitter: "https://twitter.com/BanitongwaDavid",
     resume: "https://drive.google.com/file/d/18ZKBLl_Nn7w8SASQUq4_iFm8YtjTiB9T/view?usp=drive_link"
 }
 
