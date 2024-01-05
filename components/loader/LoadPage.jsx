687983e390986d494b69c4e97698bc260f168da0
David BANITONGWA <davidbanitongwa@gmail.com>
2024-01-05 21:32:49 +0100

    loader

--- components/loader/LoadPage.jsx
+++ components/loader/LoadPage.jsx
@@ -3,8 +3,6 @@ import React, { useEffect } from "react";
 export default function LoadPage() {
 
   return (
-    <div id="preloader" className="preloaded">
-      <div className="loader_line"></div>
-    </div>
+    <div id="loader"></div>
   );
 }
