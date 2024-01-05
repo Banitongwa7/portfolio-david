7b4a2bae517115ccc3754403c9e00156dd7ee7cc
David BANITONGWA <davidbanitongwa@gmail.com>
2024-01-05 15:03:37 +0100

    delete 35 files

--- utils/Links.js
+++
@@ -1,8 +0,0 @@
-const links = {
-    "github": "https://github.com/Banitongwa7",
-    "linkedin": "https://www.linkedin.com/in/david-banitongwa/",
-    "behance": "#",
-    "twitter": "https://twitter.com/BanitongwaDavid"
-}
-
-export default links
