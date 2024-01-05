0acb76ca18ae694ee90059983b26dea1dc2fb41d
David BANITONGWA <davidbanitongwa@gmail.com>
2024-01-05 22:06:50 +0100

    update pages/blog/index.js

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -7,6 +7,21 @@ import { RxReader } from "react-icons/rx";
 
 
 export default function blog({ allPostsData }) {
+
+  const formaterURL = (url) => {
+    if (!/^https?:\/\//i.test(url)) {
+      url = "http://" + url;
+    }
+    return url;
+  }
+
+  const handleLink = (item) => {
+    if (item.link){
+      window.location.href = formaterURL(item.link)
+    }else {
+      window.location.href = `/blog/${item.id}`
+    }
+  }
   return (
     <section className="container w-full pb-[100px]">
       <h2 className="text-[30px] font-extrabold pl-10 my-[50px] mx-auto w-[80%]">
@@ -15,7 +30,7 @@ export default function blog({ allPostsData }) {
       <div>
         <ul className="grid grid-cols-1 md:grid-cols-2 gap-4 list-none w-[80%] justify-items-center mx-auto">
           {allPostsData.map((post, index) => (
-            <li key={index} className="w-[80%] rounded-md cursor-pointer shadow-lg" onClick={() => window.location.href = `/blog/${post.id}`}>
+            <li key={index} className="w-[80%] rounded-md cursor-pointer shadow-lg" onClick={() => handleLink(post)}>
               <div className="overflow-hidden rounded-t-md">
                 <Image
                   src={post.image}
@@ -31,7 +46,7 @@ export default function blog({ allPostsData }) {
                 <h3 className="text-[20px] font-medium truncate">{post.title}</h3>
                 <div className="mt-4 hover:text-green-500">
                   <Link
-                    href={`/blog/${post.id}`}
+                    href={post.link ? formaterURL(post.link) : `/blog/${post.id}`}
                     className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
                   >
                     <RxReader className="inline mr-2 mb-[2px] text-white text-[20px]" />
