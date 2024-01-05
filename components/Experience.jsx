3bfaa8f325971c56f4ed2517773541bfcb117c31
David BANITONGWA <davidbanitongwa@gmail.com>
2024-01-05 13:06:15 +0100

    delete 8 files

--- components/Experience.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Experience() {
-  return (
-    <div className='h-screen bg-amber-400'>Experience</div>
-  )
-}
--- components/Skills.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Skills() {
-
-    
-  return (
-    <div className='h-screen bg-slate-400'>Skills</div>
-  )
-}
