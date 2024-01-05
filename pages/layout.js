95559f99a481001bad187c035973afa56c998f4f
David BANITONGWA <davidbanitongwa@gmail.com>
2024-01-05 15:20:30 +0100

    metatags

--- pages/layout.js
+++ pages/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import Head from "next/head";
 const inter = Inter({ subsets: ["latin"] });
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react";
 
 export default function RootLayout({ children }) {
   const metadata = {
@@ -16,8 +16,44 @@ export default function RootLayout({ children }) {
         <meta name="title" content={metadata.title} />
         <meta name="description" content={metadata.description} />
         <link rel="icon" href="/assets/icon.png" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content="https://david-banitongwa.vercel.app/"
+        />
+        <meta property="og:title" content="Portfolio | David" />
+        <meta
+          property="og:description"
+          content="Personal Portfolio created by David Banitongwa"
+        />
+        <meta
+          property="og:image"
+          content="https://david-banitongwa.vercel.app/assets/icon.png"
+        />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://david-banitongwa.vercel.app/"
+        />
+        <meta property="twitter:title" content="Portfolio | David" />
+        <meta
+          property="twitter:description"
+          content="Personal Portfolio created by David Banitongwa"
+        />
+        <meta
+          property="twitter:image"
+          content="https://david-banitongwa.vercel.app/assets/icon.png"
+        />
       </Head>
-      <div className={inter.className} data-spy="scroll" data-target=".site-navbar-target" data-offset="300">{children}</div>
+      <div
+        className={inter.className}
+        data-spy="scroll"
+        data-target=".site-navbar-target"
+        data-offset="300"
+      >
+        {children}
+      </div>
       <Analytics />
     </>
   );
