c66c461788f79d11e27c67ae45dfbb88d811beb7
David BANITONGWA <davidbanitongwa@gmail.com>
2024-01-07 13:57:14 +0100

    update annoucement

--- components/annoucement/Annoucement.jsx
+++ components/annoucement/Annoucement.jsx
@@ -3,12 +3,20 @@ import { HiRocketLaunch } from "react-icons/hi2";
 
 export default function Annoucement() {
   return (
-    <div className="bg-gradient-to-r from-red-500 to-blue-800">
-      <div className="max-w-[85rem] px-4 py-4 sm:px-6 lg:px-8 mx-auto flex items-center justify-center">
+    <div className="bg-gray-900">
+      <div
+        id="annoucement"
+        className="max-w-[85rem] px-4 py-4 sm:px-6 lg:px-8 mx-auto flex items-center justify-center"
+      >
         <p className="me-2 font-mono inline-block text-white">
-          My personal portfolio is launched 
+          My personal portfolio is
         </p>
-        <HiRocketLaunch className="text-white text-2xl animate-[bounce_2s_ease-in-out_infinite]" />
+        <div className="words">
+          <span className="word"></span>
+          <span className="word">launched</span>
+          <span className="word">online</span>
+          <span className="word">ready</span>
+        </div>
       </div>
     </div>
   );
