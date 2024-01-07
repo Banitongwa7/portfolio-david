923aa1865e688c73a99ccf2d31f349318dfbd8dd
David BANITONGWA <davidbanitongwa@gmail.com>
2024-01-07 14:34:26 +0100

    update AllLinks.js

--- data/AllLinks.js
+++ data/AllLinks.js
@@ -2,7 +2,7 @@ const AllLinks = {
     linkedin: "https://www.linkedin.com/in/david-banitongwa/",
     github: "https://github.com/Banitongwa7",
     twitter: "https://twitter.com/BanitongwaDavid",
-    resume: "https://drive.google.com/file/d/18ZKBLl_Nn7w8SASQUq4_iFm8YtjTiB9T/view?usp=drive_link"
+    resume: "https://drive.google.com/file/d/18ZKBLl_Nn7w8SASQUq4_iFm8YtjTiB9T/view?usp=sharing"
 }
 
 export default AllLinks
