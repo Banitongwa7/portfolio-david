f81f92bcb2cb9f1c0b6617599b1766fd3569714f
David BANITONGWA <davidbanitongwa@gmail.com>
2024-01-21 18:32:33 +0100

    update customlayout.js

--- components/layout/customlayout.js
+++ components/layout/customlayout.js
@@ -7,13 +7,13 @@ export default function CustomLayout({ item, children }) {
         <meta property="og:type" content="website" />
         <meta
           property="og:url"
-          content="https://david-banitongwa.vercel.app/"
+          content="https://david-b.vercel.app/"
         />
         <meta property="og:title" content={item.title} />
         <meta property="og:description" content={item.description} />
         <meta
           property="og:image"
-          content={"https://david-banitongwa.vercel.app" + item.image}
+          content={"https://david-b.vercel.app" + item.image}
         />
         <meta property="og:site_name" content="David Banitongwa" />
         <meta property="og:locale" content="en_US" />
@@ -23,13 +23,13 @@ export default function CustomLayout({ item, children }) {
         <meta property="twitter:card" content="summary_large_image" />
         <meta
           property="twitter:url"
-          content="https://david-banitongwa.vercel.app/"
+          content="https://david-b.vercel.app/"
         />
         <meta property="twitter:title" content={item.title} />
         <meta property="twitter:description" content={item.description} />
         <meta
           property="twitter:image"
-          content={"https://david-banitongwa.vercel.app" + item.image}
+          content={"https://david-b.vercel.app" + item.image}
         />
       </Head>
       {children}
