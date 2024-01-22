62b2bccbd52c6403f482d0cc48aeddc58587b2c0
David BANITONGWA <davidbanitongwa@gmail.com>
2024-01-22 16:55:22 +0100

    update customlayout.js, layout.js and next.config.js

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -14,8 +14,8 @@ export default function RootLayout({ children }) {
         <meta name="author" content="David Banitongwa" />
         <meta charSet="utf-8" />
         <meta name="keywords" content="portfolio, david, banitongwa, developer, developer portfolio" />
-        <link rel="icon" href="https://portfolio-david-git-main-banitongwa7.vercel.app/assets/icon.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="https://portfolio-david-git-main-banitongwa7.vercel.app/assets/favicon/apple-touch-icon.png" />
+        <link rel="icon" href="/assets/icon.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon.png" />
       </Head>
       <div
         className={inter.className}
