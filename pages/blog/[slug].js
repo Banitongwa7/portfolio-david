1847c92b9d39f16e823e473c43a752509b1058bd
David BANITONGWA <davidbanitongwa@gmail.com>
2024-04-07 13:31:36 +0100

    hashnode

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,11 +1,10 @@
-//import md from "markdown-it";
 import { motion, useScroll, useSpring } from "framer-motion";
 import CustomLayout from "@/components/layout/customlayout";
 import Image from "next/image";
 import { useState } from "react";
 
 function Post({ article }) {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
+
   const { scrollYProgress } = useScroll();
   const scaleX = useSpring(scrollYProgress, {
     stiffness: 100,
@@ -19,8 +18,14 @@ function Post({ article }) {
     return new Date(date).toLocaleDateString("en-US", options);
   };
 
+  const metatags = {
+    title: article.data.publication.post.title,
+    description: article.data.publication.post.brief,
+    image: article.data.publication.post.coverImage.url,
+  };
+
   return (
-    <CustomLayout item={article} isBlog={isBlog}>
+    <CustomLayout item={metatags} isBlog={isBlog}>
       <motion.div
         className="fixed top-0 z-50 left-0 right-0 h-2 transform origin-left bg-[#05cab6]"
         style={{ scaleX }}
@@ -28,20 +33,21 @@ function Post({ article }) {
 
       <div>
         <Image
-          src={article.cover_image}
+          src={article.data.publication.post.coverImage.url}
           alt="Picture of post"
           width={1000}
           height={500}
+          priority={true}
           className="object-cover w-[80%] md:w-[60%] h-[200px] sm:h-[300px] md:h-[500px] mx-auto"
         />
         <div className="my-8 space-y-5">
           <h1 className="text-2xl md:text-3xl text-center font-extrabold dark:text-gray-100">
-            {article.title}
+            {article.data.publication.post.title}
           </h1>
           <div className="text-center text-gray-400">
             <p className="text-sm">
               <span className="font-bold">Published</span>{" "}
-              {formatDate(article.published_at)}
+              {formatDate(article.data.publication.post.publishedAt)}
             </p>
           </div>
         </div>
@@ -50,17 +56,17 @@ function Post({ article }) {
       <div className="prose pb-8 pt-5 mx-auto px-[30px] dark:prose-invert">
         <div
           className="text-[15px] md:text-[20px]"
-          dangerouslySetInnerHTML={{ __html: article.body_html }}
+          dangerouslySetInnerHTML={{ __html: article.data.publication.post.content.html }}
         />
       </div>
 
       <ul className="w-[80%] md:w-[40%] mx-auto flex flex-wrap mb-10">
-        {article.tags.map((tag, index) => (
+        {article.data.publication.post.tags.map((tag, index) => (
           <li
             key={index}
             className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
           >
-            {tag}
+            {tag.name}
           </li>
         ))}
       </ul>
@@ -71,13 +77,33 @@ function Post({ article }) {
 export default Post;
 
 export async function getStaticPaths() {
-  const res = await fetch("https://dev.to/api/articles?username=daviddb");
+  const res = await fetch("https://gql.hashnode.com", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: `
+      query Publication {
+        publication(host: "daviddb.hashnode.dev") {
+          posts(first: 10) {
+            edges {
+              node {
+                slug
+              }
+            }
+          }
+        }
+      }
+      `,
+    }),
+  });
   const allPostsData = await res.json();
 
   return {
-    paths: allPostsData.map((post) => ({
+    paths: allPostsData.data.publication.posts.edges.map((post) => ({
       params: {
-        slug: post.slug,
+        slug: post.node.slug,
       },
     })),
     fallback: false,
@@ -85,8 +111,34 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params: { slug } }) {
-  const res = await fetch(`https://dev.to/api/articles/daviddb/${slug}`);
-  console.log(slug);
+  const res = await fetch("https://gql.hashnode.com", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: `
+      query Publication {
+        publication(host: "daviddb.hashnode.dev") {
+          post(slug: "${slug}") {
+            brief
+            coverImage{
+              url
+            }
+            title
+            publishedAt
+            content {
+              html
+            }
+            tags{
+              name
+            }
+          }
+        }
+      }
+      `
+    }),
+  });
   const data = await res.json();
 
   return {
