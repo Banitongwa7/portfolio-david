76a705de6c5a9a556819e67400febbb8a9b691b7
David BANITONGWA <davidbanitongwa@gmail.com>
2024-10-27 17:27:38 +0100

    build(deps): update Skills.js, package-lock.json and pages/index.js

--- data/Skills.js
+++ data/Skills.js
@@ -1,6 +1,4 @@
-import { AiOutlineHtml5 } from "react-icons/ai";
-import { FaCss3Alt, FaNodeJs } from "react-icons/fa";
-import { IoLogoJavascript } from "react-icons/io5";
+import { FaNodeJs } from "react-icons/fa";
 import {
   FaReact,
   FaPython,
@@ -9,89 +7,90 @@ import {
   FaDatabase,
 } from "react-icons/fa6";
 import { TbBrandNextjs } from "react-icons/tb";
-import { BiLogoTailwindCss } from "react-icons/bi";
+import { BiLogoTailwindCss, BiLogoTypescript } from "react-icons/bi";
 import { DiLinux } from "react-icons/di";
 import { BsMicrosoft } from "react-icons/bs";
 import { MdOutlineDesignServices } from "react-icons/md";
 import { RiFlowChart } from "react-icons/ri";
+import { SiPowerapps, SiPowerfx } from "react-icons/si";
 
 
 const Skills = [
     {
         id: 1,
-        name: "HTML 5",
-        icon: <AiOutlineHtml5 />,
+        name: "TypeScript",
+        icon: <BiLogoTypescript />,
     },
     {
         id: 2,
-        name: "CSS 3",
-        icon: <FaCss3Alt />,
-    },
-    {
-        id: 3,
-        name: "JavaScript",
-        icon: <IoLogoJavascript />,
-    },
-    {
-        id: 4,
         name: "React JS",
         icon: <FaReact />,
     },
     {
-        id: 5,
+        id: 3,
         name: "Next JS",
         icon: <TbBrandNextjs />,
     },
     {
-        id: 6,
+        id: 4,
         name: "Node JS",
         icon: <FaNodeJs />,
     },
     {
-        id: 7,
+        id: 5,
         name: "Python",
         icon: <FaPython />,
     },
     {
-        id: 8,
+        id: 6,
         name: "Tailwind CSS",
         icon: <BiLogoTailwindCss />,
     },
     {
-        id: 9,
+        id: 7,
         name: "Bootstrap",
         icon: <FaBootstrap />,
     },
     {
-        id: 10,
+        id: 8,
         name: "Git",
         icon: <FaGitAlt />,
     },
     {
-        id: 11,
+        id: 9,
         name: "Linux",
         icon: <DiLinux />,
     },
     {
-        id: 12,
+        id: 10,
         name: "Microsoft",
         icon: <BsMicrosoft />,
     },
     {
-        id: 13,
+        id: 11,
         name: "Database SQL & NoSQL",
         icon: <FaDatabase />,
     },
     {
-        id: 14,
+        id: 12,
         name: "UI/UX Design",
         icon: <MdOutlineDesignServices />,
     },
     {
-        id: 15,
+        id: 13,
         name: "Automatization",
         icon: <RiFlowChart />,
-    }
+    },
+    {
+        id: 14,
+        name: "PowerApps",
+        icon: <SiPowerapps />,
+    },
+    {
+        id: 15,
+        name: "PowerFx",
+        icon: <SiPowerfx />,
+    },
 ]
 
 export default Skills
