13d12d89715103ccfd251146c455e4c11b01d4fe
David BANITONGWA <davidbanitongwa@gmail.com>
2025-01-10 12:56:24 +0100

    chore: remove unused files and configurations

--- components/footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Footer() {
-  return (
-    <footer className='w-full py-4 flex items-center justify-center text-[#9CA3AF] text-[14px] font-[400]'>
-        <p>{new Date().getFullYear()} Portfolio by David B.</p>
-    </footer>
-  )
-}
--- data/AllLinks.js
+++
@@ -1,7 +0,0 @@
-const AllLinks = {
-    linkedin: "https://www.linkedin.com/in/david-banitongwa/",
-    github: "https://github.com/Banitongwa7",
-    twitter: "https://twitter.com/BanitongwaDavid",
-}
-
-export default AllLinks
