d1c824f478fd9f7a2d92a5d2cad061910696ee64
David BANITONGWA <davidbanitongwa@gmail.com>
2025-02-15 16:03:50 +0100

    build(deps): update Switcher.tsx, package-lock.json and package.json

--- components/switcher/Switcher.tsx
+++ components/switcher/Switcher.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { DarkModeSwitch } from "react-toggle-dark-mode";
+//import { DarkModeSwitch } from "react-toggle-dark-mode";
 
 export default function Switcher() {
   const [darkSide, setDarkSide] = useState<boolean>(false);
@@ -13,7 +13,10 @@ export default function Switcher() {
 
   useEffect(() => {
     const root = window.document.documentElement;
-    if (window.localStorage.getItem("darkMode") == "false" || window.localStorage.getItem("darkMode") == null) {
+    if (
+      window.localStorage.getItem("darkMode") == "false" ||
+      window.localStorage.getItem("darkMode") == null
+    ) {
       root.classList.add("dark");
       setDarkSide(true);
     } else {
@@ -23,8 +26,35 @@ export default function Switcher() {
   }, [darkSide]);
 
   return (
-    <div className="p-2 bg-gray-200 dark:bg-gray-800 filter backdrop-blur rounded-lg border border-gray-300 dark:border-green-800" title="Toggle Dark Mode">
-      <DarkModeSwitch checked={darkSide} onChange={toggleDarkMode} size={20} />
-     </div>
+    <div
+      className="p-2 bg-gray-200 dark:bg-gray-800 filter backdrop-blur rounded-lg border border-gray-300 dark:border-green-800"
+      title="Toggle Dark Mode"
+    >
+      <button
+        type="button"
+        aria-label="Toggle Dark Mode"
+        onClick={() => toggleDarkMode(!darkSide)}
+        className="h-12 w-12 rounded-lg p-2 hover:bg-gray-100 dark:hover:bg-gray-700"
+      >
+        <svg
+          className="fill-violet-700 block dark:hidden"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+        >
+          <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+        </svg>
+        <svg
+          className="fill-yellow-500 hidden dark:block"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+        >
+          <path
+            d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+            fillRule="evenodd"
+            clipRule="evenodd"
+          ></path>
+        </svg>
+      </button>
+    </div>
   );
 }
