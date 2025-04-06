62b9704e91ab3bd50ac8cdd61e2f2eb7d01de685
David BANITONGWA <davidbanitongwa@gmail.com>
2025-04-06 14:49:10 +0100

    refactor: move fetchBlogPosts function inside Blog component and implement no-store cache strategy

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,8 +1,11 @@
+"use client";
+
 import type { Post } from "@/types/types";
 import BlogPostCard from "@/components/blog/BlogPostCard";
 import BlogSkeleton from "@/components/blog/BlogSkeleton";
-import { unstable_noStore as noStore } from 'next/cache';
+import { useEffect, useState } from "react";
 
+/*
 async function fetchBlogPosts() {
   try {
     const res = await fetch("https://gql.hashnode.com", {
@@ -10,6 +13,8 @@ async function fetchBlogPosts() {
       headers: {
         "Content-Type": "application/json",
       },
+      cache: 'no-store',
+      next: { revalidate: 0 },
       body: JSON.stringify({
         query: `
         query Publication {
@@ -39,8 +44,7 @@ async function fetchBlogPosts() {
           }
         }
         `,
-      }),
-      cache: 'no-store'
+      })
     });
 
     if (!res.ok) {
@@ -53,28 +57,81 @@ async function fetchBlogPosts() {
     return null;
   }
 }
+*/
 
-export default async function Blog() {
 
-  noStore();
-  const data = await fetchBlogPosts();
+export default function Blog() {
+  const [data, setData] = useState<{ data: { publication: { posts: { edges: Post[] } } } } | null>(null);
+
+  useEffect(() => {
+    async function fetchBlogPosts() {
+      try {
+        const res = await fetch("https://gql.hashnode.com", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          cache: 'no-store',
+          next: { revalidate: 0 },
+          body: JSON.stringify({
+            query: `
+            query Publication {
+              publication(host: "daviddb.hashnode.dev") {
+                posts(first: 10) {
+                  edges {
+                    node {
+                      views
+                      reactionCount
+                      coverImage{
+                        url
+                      }
+                      title
+                      readTimeInMinutes
+                      tags{
+                        name
+                      }
+                      brief
+                      slug
+                      author{
+                        profilePicture
+                        name
+                      }
+                    }
+                  }
+                }
+              }
+            }
+            `,
+          })
+        });
+
+        if (!res.ok) {
+          throw new Error('Network response was not ok');
+        }
+
+        const result = await res.json();
+        setData(result);
+      } catch (error) {
+        console.error('Error fetching blog posts:', error);
+      }
+    }
+    fetchBlogPosts();
+  }, []);
 
   if (!data) {
     return (
       <section className="w-full pb-[100px]">
-      <h2 className="text-[30px] font-extrabold pl-10 my-[50px] mx-auto w-[80%] dark:text-gray-100">
-        Blog
-      </h2>
-      <div>
-        <ul className="grid grid-cols-1 md:grid-cols-2 gap-4 list-none w-full md:w-[80%] justify-items-center mx-auto">
-        {
-          Array.from({ length: 4 }).map((_, index) => (
-            <BlogSkeleton key={index} />
-          ))
-        }
-        </ul>
-      </div>
-    </section>
+        <h2 className="text-[30px] font-extrabold pl-10 my-[50px] mx-auto w-[80%] dark:text-gray-100">
+          Blog
+        </h2>
+        <div>
+          <ul className="grid grid-cols-1 md:grid-cols-2 gap-4 list-none w-full md:w-[80%] justify-items-center mx-auto">
+            {Array.from({ length: 4 }).map((_, index) => (
+              <BlogSkeleton key={index} />
+            ))}
+          </ul>
+        </div>
+      </section>
     );
   }
 
