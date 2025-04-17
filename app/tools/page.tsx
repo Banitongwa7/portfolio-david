0497614b4c75600c23c637e5f8cd8930b1e3fc38
David BANITONGWA <davidbanitongwa@gmail.com>
2025-04-17 23:51:39 +0100

    feat: implement CardTool component and update Tools page layout

--- app/tools/page.tsx
+++ app/tools/page.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import CardTool from '@/components/alltools/CardTool'
 
 export default function Tools() {
   return (
-    <div className='h-screen text-center text-3xl font-bold text-white'>All tools here</div>
+    <div>
+        <h2 className='text-center text-3xl font-bold text-white mt-10'>All tools here</h2>
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-10 md:w-[80%] justify-items-center mx-auto mt-10'>
+            <CardTool />
+            <CardTool />
+            <CardTool />
+            <CardTool />
+            <CardTool />
+            <CardTool />
+            <CardTool />
+        </div>
+    </div>
   )
 }
---
+++ components/alltools/CardTool.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function CardTool() {
+  return (
+    <div className="relative flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+      <div className="p-6">
+        <h5 className="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+          Tool title
+        </h5>
+        <p className="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </div>
+      <div className="p-6 pt-0">
+        <button
+          className="select-none rounded-lg bg-pink-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+          type="button"
+          data-ripple-light="true"
+        >
+          Read More
+        </button>
+      </div>
+    </div>
+  );
+}
