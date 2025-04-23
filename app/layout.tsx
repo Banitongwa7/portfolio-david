d2d8c2ebb5afb24f72929930e27be94d6d7018e9
David BANITONGWA <davidbanitongwa@gmail.com>
2025-04-23 14:27:36 +0100

    fix: remove unnecessary data-message attribute from BMC-Widget script

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,7 +59,7 @@ export default function RootLayout({
         <Footer />
         <Analytics />
         <SpeedInsights />
-        <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="dbanitongwa" data-description="Support me on Buy me a coffee!" data-message="Thanks for your support 💖" data-color="#40DCA5" data-position="Right" data-x_margin="18" data-y_margin="18" defer></script>
+        <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="dbanitongwa" data-description="Support me on Buy me a coffee!" data-color="#40DCA5" data-position="Right" data-x_margin="18" data-y_margin="18" defer></script>
       </body>
     </html>
   );
