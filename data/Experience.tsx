76764b8e5b3493aee48d56844a37aa3fef21ee3f
Banitongwa7 <davidbanitongwa@gmail.com>
2025-10-24 15:06:19 +0100

    update Experience.tsx

--- data/Experience.tsx
+++ data/Experience.tsx
@@ -7,7 +7,7 @@ const Experience = [
         date: "12/2024 - Now",
         position: "Technical Consultant",
         company: "Biware",
-        location: "Tunis/Tunisie",
+        location: "Tunis, Tunisia",
         icon: Tunisia,
     },
     {
@@ -15,20 +15,21 @@ const Experience = [
         date: "02/2023 - 08/2024",
         position: "Full Stack Web Developer",
         company: "Synapse-HR",
-        location: "Tunis/Tunisie",
+        location: "Tunis, Tunisia",
         icon: Tunisia,
     },
     {
         id: 3,
         date: "10/2022 - 09/2024",
-        position: "Internship - Consultant Microsoft Power Platform",
+        position: "Internship - Microsoft Power Platform Consultant",
         company: "Biware",
-        location: "Tunis/Tunisie",
+        location: "Tunis, Tunisia",
         icon: Tunisia,
-    },{
+    },
+    {
         id: 4,
         date: "09/2022 - 08/2023",
-        position: "React JS developer - Freelance",
+        position: "React JS Developer - Freelance",
         company: "BhetiConnect",
         location: "France",
         icon: France,
@@ -36,27 +37,27 @@ const Experience = [
     {
         id: 5,
         date: "02/2022 - 06/2022",
-        position: "Full Stack Web Developer - Intern",
+        position: "Full Stack Web Developer Intern",
         company: "ITC",
-        location: "Tunis/Tunisie",
+        location: "Tunis, Tunisia",
         icon: Tunisia,
     },
     {
         id: 6,
         date: "07/2021 - 10/2021",
-        position: "Assistant Technique",
+        position: "Technical Assistant",
         company: "Allo PC",
-        location: "Tunis/Tunisie",
+        location: "Tunis, Tunisia",
         icon: Tunisia,
     },
     {
         id: 7,
         date: "08/2020 - 11/2020",
-        position: "SAP Next-Gen Challenge d'été",
-        company: "Douane Tunisienne",
-        location: "Tunis/Tunisie",
+        position: "SAP Next-Gen Summer Challenge",
+        company: "Tunisian Customs",
+        location: "Tunis, Tunisia",
         icon: Tunisia,
     }
-]
+];
 
 export default Experience
