a52c39993211b7c507c6052b9b7020b3c784e80f
Banitongwa7 <davidbanitongwa@gmail.com>
2026-09-15 09:36:11 +0100

    update

--- data/Experience.tsx
+++ data/Experience.tsx
@@ -1,17 +1,26 @@
 import France from "../public/assets/france.svg"
 import Tunisia from "../public/assets/tunisia.svg"
+import DRCongo from "../public/assets/dr-congo.svg"
 
 const Experience = [
     {
         id: 1,
-        date: "12/2024 - Now",
+        date: "04/2026 - Now",
+        position: "IT Engineer",
+        company: "Sofibanque",
+        location: "Kinshasa, DR Congo",
+        icon: DRCongo,
+    },
+    {
+        id: 2,
+        date: "12/2024 - 12/2025",
         position: "Technical Consultant",
         company: "Biware",
         location: "Tunis, Tunisia",
         icon: Tunisia,
     },
     {
-        id: 2,
+        id: 3,
         date: "02/2023 - 08/2024",
         position: "Full Stack Web Developer",
         company: "Synapse-HR",
@@ -19,7 +28,7 @@ const Experience = [
         icon: Tunisia,
     },
     {
-        id: 3,
+        id: 4,
         date: "10/2022 - 09/2024",
         position: "Internship - Microsoft Power Platform Consultant",
         company: "Biware",
@@ -27,7 +36,7 @@ const Experience = [
         icon: Tunisia,
     },
     {
-        id: 4,
+        id: 5,
         date: "09/2022 - 08/2023",
         position: "React JS Developer - Freelance",
         company: "BhetiConnect",
@@ -35,7 +44,7 @@ const Experience = [
         icon: France,
     },
     {
-        id: 5,
+        id: 6,
         date: "02/2022 - 06/2022",
         position: "Full Stack Web Developer Intern",
         company: "ITC",
@@ -43,7 +52,7 @@ const Experience = [
         icon: Tunisia,
     },
     {
-        id: 6,
+        id: 7,
         date: "07/2021 - 10/2021",
         position: "Technical Assistant",
         company: "Allo PC",
@@ -51,7 +60,7 @@ const Experience = [
         icon: Tunisia,
     },
     {
-        id: 7,
+        id: 8,
         date: "08/2020 - 11/2020",
         position: "SAP Next-Gen Summer Challenge",
         company: "Tunisian Customs",
